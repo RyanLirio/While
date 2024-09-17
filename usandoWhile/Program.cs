03ca86900d2ab0f313d98a6377c44262893823eb
Ryan Lirio <ryanlirio2@gmail.com>
2024-09-17 11:11:30 -0300

    usandoOwhile

---
+++ usandoWhile/Program.cs
@@ -0,0 +1,20 @@
+﻿﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i = 6;
+        while (i > 1)
+        {
+            Console.WriteLine (i);
+            i--;
+        }
+        int a = 3;
+        while (a < 6)
+        {
+            a++;
+            Console.WriteLine (a);
+        }
+    }
+}
